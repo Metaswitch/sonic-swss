f4dec6278a7e6473422bb59ea12bf42075a6e4a2
Marius Supuran <marius.supuran@metaswitch.com>
2021-05-13 12:47:51 +0300

    [orchagent] New version of the copy function also uses address mask * Added a new version of the copy function that also takes the address mask as a parameter * This new version is used in intfsorch in the Ip2MeRoute functions * This is required in order to be able to correctly add subnet routes

--- orchagent/swssnet.h
+++ orchagent/swssnet.h
@@ -76,6 +76,28 @@ inline static sai_ip_prefix_t& copy(sai_ip_prefix_t& dst, const IpAddress& src)
     return dst;
 }
 
+inline static sai_ip_prefix_t& copy(sai_ip_prefix_t& dst, const IpAddress& src, const IpAddress& mask)
+{
+    auto sip = src.getIp();
+    auto msk = mask.getIp();
+    switch(sip.family)
+    {
+        case AF_INET:
+            dst.addr_family = SAI_IP_ADDR_FAMILY_IPV4;
+            dst.addr.ip4 = sip.ip_addr.ipv4_addr;
+            dst.mask.ip4 = msk.ip_addr.ipv4_addr;
+            break;
+        case AF_INET6:
+            dst.addr_family = SAI_IP_ADDR_FAMILY_IPV6;
+            memcpy(dst.addr.ip6, sip.ip_addr.ipv6_addr, 16);
+            memcpy(dst.mask.ip6, msk.ip_addr.ipv6_addr, 16);
+            break;
+        default:
+            throw std::logic_error("Invalid family");
+    }
+    return dst;
+}
+
 inline static sai_ip_prefix_t& subnet(sai_ip_prefix_t& dst, const sai_ip_prefix_t& src)
 {
     dst.addr_family = src.addr_family;
